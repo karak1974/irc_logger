ecf60886d8ddc64b2aae672eeb94ab3792c99e92
karak1974 <wolfy42@protonmail.com>
2023-07-13 02:03:23 +0200

    Refactor, test in prod

--- main.go
+++ main.go
@@ -3,47 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"os"
+	"net"
 
 	hbot "github.com/whyrusleeping/hellabot"
+	log "gopkg.in/inconshreveable/log15.v2"
 )
 
-var serv = flag.String("server", "irc.wolfy.me:6667", "hostname and port for irc server to connect to")
-var nick = flag.String("nick", "irc_bot", "nickname for the bot")
+var serv = flag.String("server", "irc.ircnet.com:6667", "hostname and port for irc server to connect to")
+var nick = flag.String("nick", "camp_printer", "nickname for the bot")
+var enters = flag.Int("enters", 7, "New lines before messages")
 
-// -info trigger
-/*
-var infoMessage = hbot.Trigger{
-	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && m.Content == "-info"
-	},
-	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
-		irc.Reply(m, "An IRC bot that do logging")
-		return false
-	},
-}
-*/
+func printMessage(msg string) {
+	conn, err := net.Dial("tcp", "172.16.0.28:9100")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
 
-// Logger
-var logger = hbot.Trigger{
-	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && m.Content != "-info"
-	},
-	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
-		server := *serv
-		go func(server string, channel string, user string, content string) {
-			file, err := os.OpenFile("/var/log/"+server+"."+channel[1:]+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.SetOutput(file)
-
-			fmt.Println("<"+user+">: "+content)
-			log.Println("<"+user+">: "+content)
-		}(server[:len(server)-5], m.To, m.From, m.Content)
-		return false
-	},
+	// Print the message
+	_, err = fmt.Fprintf(conn, "%s\n", msg)
+	if err != nil {
+		fmt.Println("Error sending data:", err)
+		return
+	}
+	// This is necessarry to see the message on the printer
+	for i := 0; i < *enters; i++ {
+		_, err = fmt.Fprintf(conn, "\n")
+		if err != nil {
+			fmt.Println("Error sending new line:", err)
+			return
+		}
+	}
 }
 
 func main() {
@@ -53,16 +44,46 @@ func main() {
 		bot.HijackSession = true
 	}
 	channels := func(bot *hbot.Bot) {
-		bot.Channels = []string{"#channel1", "#channel2"}
+		bot.Channels = []string{"#camp++"}
 	}
 	irc, err := hbot.NewBot(*serv, *nick, hijackSession, channels)
 	if err != nil {
 		panic(err)
 	}
 
-	//irc.AddTrigger(infoMessage)
-	irc.AddTrigger(logger)
+	irc.AddTrigger(sayInfoMessage)
+	irc.AddTrigger(logMessage)
+	irc.Logger.SetHandler(log.StdoutHandler)
 
 	irc.Run()
 	fmt.Println("Bot shutting down.")
+}
+
+var sayInfoMessage = hbot.Trigger{
+	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
+		return m.Command == "PRIVMSG" && m.Content == "-info"
+	},
+	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
+		// Reply, but also logged
+		irc.Reply(m, "I'm a printer in the hackcenter, Wolfy is my creator")
+		return false
+	},
+}
+
+var logMessage = hbot.Trigger{
+	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
+		return m.Command == "PRIVMSG"
+	},
+	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
+		var msg string
+		if m.To == *nick {
+			msg = fmt.Sprintf("<%s><%s>: %s", "Private", m.From, m.Content)
+		} else {
+			msg = fmt.Sprintf("<%s><%s>: %s", m.To, m.From, m.Content)
+		}
+
+		fmt.Println(msg)
+		printMessage(msg)
+		return false
+	},
 }
